39548cb421778293e6ad3bf0eb5d13576a83fddf
borya <borisahadov@gmail.com>
2026-02-06 15:37:57 +0300

    Fix CSS and Fonts for GitHub Pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,16 +35,51 @@ export const metadata: Metadata = {
   },
 }
 
+import localFont from 'next/font/local'
+import { basePath } from '@/lib/basePath'
+
+const scandia = localFont({
+  src: [
+    {
+      path: '../../public/fonts/scandia/Scandia-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/scandia/Scandia-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/scandia/Scandia-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-scandia',
+})
+
+const overpass = localFont({
+  src: [
+    {
+      path: '../../public/fonts/overpass/OverpassMono-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-overpass',
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${scandia.variable} ${overpass.variable}`}>
       <head>
-        <link rel="stylesheet" href="/old-site.css" />
-        <link rel="stylesheet" href="/header-override.css" />
-        <link rel="stylesheet" href="/mobile-figma-styles.css" />
-        <link rel="stylesheet" href="/cookie-banner.css" />
+        <link rel="stylesheet" href={`${basePath}/old-site.css`} />
+        <link rel="stylesheet" href={`${basePath}/header-override.css`} />
+        <link rel="stylesheet" href={`${basePath}/mobile-figma-styles.css`} />
+        <link rel="stylesheet" href={`${basePath}/cookie-banner.css`} />
       </head>
-      <body>
+      <body style={{ fontFamily: 'var(--font-scandia), sans-serif' }}>
         <AppProvider>
           {children}
           <CookieBanner />
